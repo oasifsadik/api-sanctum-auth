1303b696c70c2f4b2387d482bb672cc3d4891c7a
oasifsadik <oasifsadik99@gmail.com>
2024-10-09 14:18:59 +0600

    post crud using api

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Http\Requests\StorePostRequest;
-use App\Http\Requests\UpdatePostRequest;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
@@ -22,7 +20,18 @@ class PostController extends Controller implements HasMiddleware
      */
     public function index()
     {
-        return Post::all();
+        try {
+            $data = Post::orderBy('id','DESC')->get();
+            return response()->json([
+                'status' => true,
+                'data'   => $data,
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status'  => false,
+                'message' => $exception->getMessage()
+            ]);
+        }
     }
 
     /**
@@ -30,35 +39,92 @@ class PostController extends Controller implements HasMiddleware
      */
     public function store(Request $request)
     {
-          $save =  $request->validate([
-                'title' => 'required',
-                'description' => 'required',
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+        try {
+            $post = $request->user()->posts()->create([
+                'title' => $request->title,
+                'description' => $request->description,
             ]);
-        $post = $request->user()->posts()->create($save);
-        return  $post;
+            return response()->json([
+                'status' => true,
+                'post'  => $post,
+                'message' => 'Post Store Successfully',
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ]);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show($id)
     {
-        //
+        try {
+            $post = Post::find($id);
+            return response()->json([
+                'status' => true,
+                'post'  => $post,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ]);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, $id)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+        try {
+            $post = $request->user()->posts()->findOrFail($id);
+            $post->update([
+                'title' => $request->title,
+                'description' => $request->description,
+            ]);
+            return response()->json([
+                'status' => true,
+                'post'  => $post,
+                'message' => 'Post Update Successfully',
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy($id)
     {
-        //
+        try {
+            $post = Post::find($id);
+            $post->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'Employee delete successfully',
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,17 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-Route::apiResource('posts', PostController::class);
+// Route::apiResource('posts', PostController::class);
 
 Route::post('login', [AuthController::class,'login']);
 Route::post('register', [AuthController::class,'register']);
 
-Route::post('logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::apiResource('posts', PostController::class);
+});
+
+// Route::post('logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
 
 // Route::get('/', function () {
 //     return "jidsan";
