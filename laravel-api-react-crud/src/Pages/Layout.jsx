5f57556042724c6d08870a7b3d5265b9a89a19c2
Jisan <oasifsadik99@gmail.com>
2024-10-09 22:29:51 +0600

    design navbar

--- laravel-api-react-crud/src/Pages/Layout.jsx
+++ laravel-api-react-crud/src/Pages/Layout.jsx
@@ -2,11 +2,12 @@ import { Link, Outlet } from "react-router-dom"
 export default function Layout(){
     return(
         <>
-        <header>
-            <nav>
-                <Link to="/" className="nav-link">Home</Link>
+        <header className="bg-slate-800 shadow-md py-4">
+            <nav className="max-w-screen-lg mx-auto flex items-center justify-between px-5">
+                <Link to="/" className="nav-link text-white hover:text-blue-400 transition duration-200">Home</Link>
             </nav>
         </header>
+
         <main>
             <Outlet/>
         </main>
